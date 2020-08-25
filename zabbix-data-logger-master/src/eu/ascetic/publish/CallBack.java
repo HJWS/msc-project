7e070bb9effc929b5d23e54a8b2f46810161b8cc
HJWS <sirbrucewang@gmail.com>
2020-08-25 18:42:11 +0100

    Add files via upload

---
+++ zabbix-data-logger-master/src/eu/ascetic/publish/CallBack.java
@@ -0,0 +1,21 @@
+package eu.ascetic.publish;
+
+
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class CallBack implements MqttCallback{
+    public void connectionLost(Throwable throwable) {
+
+    }
+
+    public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+        //System.out.println("Receive:\n" + "Topic: " + topic + "\n" + "Content" + new String(mqttMessage.getPayload()));
+    }
+
+    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+    }
+}
